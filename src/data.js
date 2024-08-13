d7a045cbea00f123c3d43be910bc0039bfcdf767
Indiana Namaul <91829683+culxe@users.noreply.github.com>
2024-08-13 17:23:20 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "Sentiment Analysis for Google Maps Reviews",
+    subtitle: "Natural Language Processing",
+    description: "The tourism application finder with sentiment analysis (NLP) using Tensorflow. Model develop from data scratch, CNN model, and achieved 80% accuracy",
+    image: "./project-1.png",
+    link: "https://github.com/BangkitCapstoneFIT/ML-findindonesiatourism",
+  },
+  {
+    title: "Reidentification Person using Transformers",
+    subtitle: "Computer Vision",
+    description:
+      "Developed a machine learning model for person re-identification using Vision Transformer with the PyTorch framework, analyzed model performance, and visualized the model using Grad-CAM. The testing conducted showed better performance from a previous research models (PFNet Network).",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Tableau Projects",
+    subtitle: "Tableau Projects",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "Freecodecamp Certifications Projects",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = ["Python", "HTML CSS", "Natural Language Processing", "Computer Vision", "Project Management", "dfdfd"];
