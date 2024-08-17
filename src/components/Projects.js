137f14f556b3802effb1c9fa756fa2fc818666fd
Indiana Namaul <91829683+culxe@users.noreply.github.com>
2024-08-17 22:12:58 +0700

    all commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ export default function Projects() {
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4 text-white" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">My Projects..</h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">There are my projects while studying in Institut Teknologi Sepuluh Nopember</p>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">These are my projects while studying in Institut Teknologi Sepuluh Nopember</p>
         </div>
         <div className="flex flex-wrap -m-6">
           {projects.map((project) => (
